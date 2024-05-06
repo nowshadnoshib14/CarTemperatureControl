87ce957a27ee29dccc6dfd287efd60f0ddecc53b
Nowshad <nowshadnoshib1802@gmail.com>
2024-05-06 22:11:24 +0600

    first commit

--- src/RangeInput.java
+++ src/RangeInput.java
@@ -1,2 +1,29 @@
-package PACKAGE_NAME;public class RangeInput {
+public class RangeInput {
+    private int currentTemperature, minTemperature, maxTemperature;
+
+    public RangeInput(){
+        minTemperature = 60;
+        maxTemperature = 80;
+        calculateCurrentTemperature();
+    }
+    public RangeInput(int min, int max){
+        minTemperature = min;
+        maxTemperature = max;
+        calculateCurrentTemperature();
+    }
+
+    private void calculateCurrentTemperature() {
+        currentTemperature = (minTemperature + maxTemperature) / 2;
+    }
+
+    public void up(){
+        currentTemperature = Math.min(currentTemperature + 1, maxTemperature);
+    }
+    public void down(){
+        currentTemperature = Math.max(currentTemperature - 1, minTemperature);
+    }
+    public int getCurrentTemperature(){
+        return currentTemperature;
+    }
+
 }
